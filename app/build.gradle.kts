2797f4298beafd5240dd9a71205b78396667d41d
tezeku <cuzyuhin@yandex.ru>
2026-05-03 12:08:19 +0300

    feat(auth): integrate auth API and verify end-to-end flow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
-        buildConfigField("String", "BASE_URL", "\"http://10.0.2.2:8080/api/v1\"")
+        buildConfigField("String", "BASE_URL", "\"http://10.0.2.2:8080/api/v1/\"")
     }
 
     buildTypes {
--- app/src/main/java/ru/akuzyukhin/orientir/MainActivity.kt
+++ app/src/main/java/ru/akuzyukhin/orientir/MainActivity.kt
@@ -1,11 +1,21 @@
 package ru.akuzyukhin.orientir
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import ru.akuzyukhin.orientir.feature.auth.domain.repository.AuthRepository
 import ru.akuzyukhin.orientir.ui.theme.OrientirTheme
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -14,7 +24,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             OrientirTheme {
-
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Text(
+                        text = "Orientir",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
             }
         }
     }
